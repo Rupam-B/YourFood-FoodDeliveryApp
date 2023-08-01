01ce34d3b3b357d9f602b23ec10b6666c48bb2c3
Rupam-B <rupam.banerje@gmail.com>
2023-08-02 02:03:03 +0530

    button text change

--- src/components/Restaurants/Restaurant.jsx
+++ src/components/Restaurants/Restaurant.jsx
@@ -89,7 +89,7 @@ const Restaurant = () => {
           <h3 className="mb-0 text-white pb-2">{item.Name}</h3>
           <div className="mb-1 body-secondary text-danger">Rating {item.rating}</div>
           <p className="card-text mb-auto pb-2 text-white">{item.menu}</p>
-          <button className='btn btn-dark w-50 mt-2'>Order Now</button>
+          <button className='btn btn-dark w-50 mt-2'>Check Details</button>
         </div>
         <div className="col-auto d-lg-block">
           <img src={item.src} className="bd-placeholder-img Rest-Card-img" alt="..."  />
