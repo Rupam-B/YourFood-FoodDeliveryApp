071cc9747d7deec574898ad0b375f848dc7c36cb
Rupam-B <rupam.banerje@gmail.com>
2023-08-02 12:26:42 +0530

    media revised 5

--- src/components/Restaurants/Restaurant.jsx
+++ src/components/Restaurants/Restaurant.jsx
@@ -71,16 +71,16 @@ const Restaurant = () => {
 ]
   return (
     <>
-    <div className="p-5 p-md-5 mb-1 rounded text-body-emphasis bg-dark">
+    <div className="p-5 p-md-5 mb-1 rounded text-body-emphasis bg-dark"  style={{width:'100%',overflow:'hidden'}}>
     <div className="col-lg-6 px-0">
       <h1 style={{paddingTop:'2rem' , fontWeight:'bolder'}} className="display-4 fst-italic text-danger">Top Restaurants</h1>
       <p className="lead my-3 text-white">We serve you with the quality food from the top picks of most of the customer and gurantees to deliver on time</p>
       <br /><br />
       <p className="lead mb-0 text-white"><a href="/" className="text-body-emphasis fw-bold text-decoration-none text-primary">Explore...</a></p>
     </div>
-    <img src="/assets/Restaurant9.jpg" alt="" srcset=""  className='Restimg'/>
+    <img src="/assets/Restaurant10.jpg" alt="" srcset=""  className='Restimg'/>
   </div>
-  <div className="row mb-2 black-bgrnd p-4" style={{width:'100vw', overflow:'hidden'}}>
+  <div className="row mb-2 black-bgrnd p-4" style={{width:'100%', overflow:'hidden'}}>
     {restaurantData.map((item)=>( 
     <div className="col-md-6 pt-5" key={item.id}>
       <div className="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
@@ -89,7 +89,7 @@ const Restaurant = () => {
           <h3 className="mb-0 text-white pb-2">{item.Name}</h3>
           <div className="mb-1 body-secondary text-danger">Rating {item.rating}</div>
           <p className="card-text mb-auto pb-2 text-white">{item.menu}</p>
-          <button className='btn btn-dark w-50 mt-2'>Check Details</button>
+          <button className='btn btn-dark w-50 mt-2 text-primary'>Details</button>
         </div>
         <div className="col-auto d-lg-block">
           <img src={item.src} className="bd-placeholder-img Rest-Card-img" alt="..."  />
